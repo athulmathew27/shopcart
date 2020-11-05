17382323f860db2dbf8b8dc23b08bb9a5084b1ab
athulmathrw27 <athulmathew27@gmail.com>
2020-11-05 17:18:34 +0530

    initial depoy

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,7 +4,9 @@ import { LoginComponent } from './components/login/login.component';
 import { ManageAddressComponent } from './components/manage-address/manage-address.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { AngularFireAuthGuard, AngularFireAuthGuardModule, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
+const redirectUnauthorizedToLogin = ()=> redirectUnauthorizedTo(['/auth/login']);
 const routes: Routes = [
   {
     path:'login',
@@ -16,10 +18,14 @@ const routes: Routes = [
   },
   {
     path: 'profile',
+    canActivate : [AngularFireAuthGuard],
+    data : { authGuardPipe : redirectUnauthorizedToLogin},
      component: ProfileComponent
   },
   {
     path: 'manage_address',
+    canActivate : [AngularFireAuthGuard],
+    data : { authGuardPipe : redirectUnauthorizedToLogin},
      component: ManageAddressComponent
   }
 ];
